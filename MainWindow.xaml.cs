20d0234be2a8ff238f2499995cf8075811415706
Машулька Машулька <marussia01@gmail.com>
2022-11-17 11:55:11 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WpfDamagCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        SwordDamage swordDamage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            swordDamage = new SwordDamage(random.Next(1,7)+ random.Next(1, 7)+ random.Next(1, 7));
+            RollDice();
+        }
+
+        public void RollDice()
+        {
+            swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            DisplayDamagr();
+        }
+        void DisplayDamagr()
+        {
+            damage.Text = $"Rolled {swordDamage.Roll} for {swordDamage.Damage} HP";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+        private void flaming_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Flaming = true;
+            DisplayDamagr();
+        }
+        private void flaming_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Flaming = false;
+            DisplayDamagr();
+        }
+        private void magic_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Magic = true;
+            DisplayDamagr();
+        }
+        private void magic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.Magic = false;
+            DisplayDamagr();
+        }
+    }
+}
---
+++ SwordDamage.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;//для работы с выводом строк в окно вывода
+
+namespace WpfDamagCalculator
+{
+        internal class SwordDamage
+        {
+            private const int BASE_DAMAGE = 3;
+            private const int FLAME_DAMAGE = 2;
+            /// <summary>
+            /// Константы калькулятора дамага
+            /// </summary>
+            private int roll;
+            /// <summary>
+            /// Устанавливает или получает бросок кубика 3на6
+            /// </summary>
+            public int Roll
+            {
+                get { return roll; }
+                set { roll = value; CalculatrDamage(); }
+            }
+
+            public int Damage { get; private set; }
+            /// <summary>
+            /// Вычисляет поражение в зависимости от текущих свойств
+            /// </summary>
+            public void CalculatrDamage()
+            {
+                decimal MagicMultiplier = 1M;
+                if (Magic) MagicMultiplier = 1.75M;
+
+                Damage = BASE_DAMAGE;
+                Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE;
+
+                if (Flaming) Damage += FLAME_DAMAGE;
+            }
+            private bool magic;
+            /// <summary>
+            /// Если меч волшебный true, иначе false
+            /// </summary>
+            public bool Magic
+            {
+                get { return magic; }
+                set { magic = value; CalculatrDamage(); }
+            }
+            public bool flaming;
+            /// <summary>
+            /// Если меч огненный true, иначе false
+            /// </summary>
+            public bool Flaming
+            {
+                get { return flaming; }
+                set { flaming = value; CalculatrDamage(); }
+            }
+            /// <summary>
+            /// Вычисление поражения для занчений magic и flaminf по умолчанию и нач броска 3на6
+            /// </summary>
+            /// <param name="startindRoll"> Начальный бросок 3на6</param>
+            public SwordDamage(int startindRoll)
+            {
+                roll = startindRoll;
+                CalculatrDamage();
+            }
+        }
+    }
